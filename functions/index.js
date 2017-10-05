f3dd8d05baf59696237b9a0d544de68e444e1675
Joe Sullivan <josulliv101@gmail.com>
2017-10-05 17:49:44 -0400

    universal bare bones app

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const app = require('express')();
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+const ServerApp = React.createFactory(require('./build/server.bundle.js').default);
+
+const renderApplication = (url, res, initialState = {}) => {
+  const html = ReactDOMServer.renderToString(ServerApp({url: url, context: {}, initialState}));
+  res.send(html);
+};
+
+app.get('/favicon.ico', function(req, res) {
+  res.send(204);
+});
+
+app.get('/:userId?', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=60, s-maxage=180');
+  renderApplication(req.url, res, {foo: "bar"});
+});
+
+exports.app = functions.https.onRequest(app);

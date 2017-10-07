e2b05d3a32ea76c0ba10dec77ee6df5a14c80cd9
Joe Sullivan <josulliv101@gmail.com>
2017-10-07 13:50:29 -0400

    jss and stylesheetreigistry error fix

--- functions/render.js
+++ functions/render.js
@@ -3,27 +3,21 @@ const serialize = require('serialize-javascript');
 const CleanCSS = require('clean-css');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const {ServerApp, getSheetsRegistry} = require('./build/server.bundle.js');
-// const ServerApp = require('./build/server.bundle.js').default;
-const template = require('./template');
 //
+const {ServerApp, getSheetsRegistry} = require('./build/server.bundle.js');
+const template = require('./template');
+
 const App = React.createFactory(ServerApp);
 const SheetsRegistry = getSheetsRegistry();
 
-// const ServerApp = require('./build/server.bundle.js').default;
-
-console.log('getSheetsRegistry', ServerApp, getSheetsRegistry)
-
 module.exports = (url, res, initialState) => {
   const sheets = new SheetsRegistry();
-  const props = {
+  const html = ReactDOMServer.renderToString(App({
     context: {},
     initialState,
     sheets,
     url,
-  };
-
-  const html = ReactDOMServer.renderToString(App(props));
+  }));
   const templatedHtml = template({
     assets: functions.config().assets,
     body: html,
